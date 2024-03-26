8a18af7dbbe363253806a92089dda49cc67bebfb
harfy345 <hakam.almotlk345@gmail.com>
2024-03-26 13:58:28 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -140,7 +140,7 @@ import {
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
-        "Design and develop complex system as a ticketing system, etc. ",
+        "Design and develop complex system as a ticketing system, etc.",
       ],
     },
   ];
