8f0e1223507a0df43908b439c9a08295bf1279e7
harfy345 <hakam.almotlk345@gmail.com>
2024-03-27 12:37:44 -0400

    Revert "mobile test REVERT"
    This reverts commit 55afae4814ab5b84cbd71a71219789e40875acb5.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,8 +4,8 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const { scene } = useGLTF("./desktop_pc/scene.gltf");
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
@@ -19,21 +19,39 @@ const Computers = () => {
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
-      {scene && (
-        <primitive
-          object={scene}
-          scale={ 0.75}
-          position={[0, -3.25, -1.5]}
-          rotation={[-0.01, -0.2, -0.1]}
-        />
-      )}
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
- 
- 
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <Canvas
@@ -49,7 +67,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers  />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
